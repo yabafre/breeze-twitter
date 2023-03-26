01bc204b69f76d8fab4821a30fce9c2db1ae59d2
Frédéric Yaba <frederic@bobochicparIs.com>
2023-03-26 09:37:11 +0200

    Update https-75

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,6 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
