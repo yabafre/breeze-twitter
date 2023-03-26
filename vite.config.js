3a697804236f61c7a73ed1577f77c50922611a68
Frédéric Yaba <frederic@bobochicparIs.com>
2023-03-26 09:21:01 +0200

    Update https-73

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig(({ mode }) => {
-    const isProduction = mode === 'production';
-    const base = isProduction ? 'https://' : '';
-
-    return {
-        base,
-        plugins: [
-            laravel({
-                input: [
-                    'resources/css/app.css',
-                    'resources/js/app.js',
-                ],
-                refresh: true,
-            }),
-        ],
-    };
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+    server: {
+        https: true
+    }
 });
