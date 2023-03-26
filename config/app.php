df99c7c2619cfbcd83816ed47aa4fb7d44452980
Frédéric Yaba <frederic@bobochicparIs.com>
2023-03-26 09:31:42 +0200

    Update https-74

--- config/app.php
+++ config/app.php
@@ -54,9 +54,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://twitter-12ur.onrender.com'),
 
-    'asset_url' => env('ASSET_URL'),
+    'asset_url' => env('ASSET_URL', 'https://twitter-12ur.onrender.com'),
 
     /*
     |--------------------------------------------------------------------------
