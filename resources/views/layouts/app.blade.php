42455d07527b29baa8e7834d5dfdd51a55190240
Frédéric Yaba <frederic@bobochicparIs.com>
2023-03-26 08:47:05 +0200

    Update blade-2 css js https-2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-        <script src="{{ asset('js/app.js') }}"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
