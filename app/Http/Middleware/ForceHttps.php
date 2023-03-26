71ccc6d981dbdac6c6b4f813ca9827bd459d3ecd
Frédéric Yaba <frederic@bobochicparIs.com>
2023-03-26 14:53:04 +0200

    Update https-77

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -4,18 +4,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Symfony\Component\HttpFoundation\Response;
 
 class ForceHttps
 {
     /**
      * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next)
     {
-        if (app()->environment('production') && $request->header('X-Forwarded-Proto') !== 'https') {
+        if (!$request->secure() && App::environment(['production'])) {
             return redirect()->secure($request->getRequestUri());
         }
 
