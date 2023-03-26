0bdba508a0e6916598f30dabc03a38951d101e3e
Frédéric Yaba <frederic@bobochicparIs.com>
2023-03-26 08:41:27 +0200

    Update blade css js https-2

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    base: process.env.NODE_ENV === 'production' ? 'https://twitter-12ur.onrender.com/' : '/',
-    server: {
-        host: '0.0.0.0',
-        port: 3000,
-        strictPort: true,
-        https: true,
-    },
+export default defineConfig(({ command }) => {
+    const isProduction = command === 'build';
+    const base = isProduction ? 'https://twitter-12ur.onrender.com/' : '/';
+
+    return {
+        base,
+        plugins: [
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+    };
 });
